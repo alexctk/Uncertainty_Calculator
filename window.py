ea5560f76f706fc5b9dc8dc1100662797a1114ad
alkong <alexanderlkong@gmail.com>
2018-06-09 11:00:34 -0700

    Adjusted window layout

--- window.py
+++ window.py
@@ -14,21 +14,21 @@ bottomframe.pack(side = "bottom")
 # Widget syntax: variable_name = Widget_type(parent_widget, option1, option2, ...)
 
 # text box
-text_entry = Entry(frame, width = 20) # entry type widget
-text_entry.pack(side = "left") # widget placement
+text_entry = Entry(frame, width = 50) # entry type widget
+text_entry.pack(expand=True) # widget placement
 
 # take string input and pass to calc module
 # obtain result from calc module
 def compute():
     text_input = text_entry.get()
     result = c.eval_input(text_input)
-    print(result)
+    print(result) # debugging purposes
     result_label.configure(text=result.format(result))
 
 compute = Button(frame, text = " calculate ", command = compute)
-compute.pack(side='left', padx=4)
+compute.pack(expand=True)
 
-result_label = Label(frame, width = 20)
+result_label = Label(frame, width = 50)
 result_label.pack(side = "left")
 
 quit_button = Button(bottomframe, text = "Quit", command=frame.quit)
